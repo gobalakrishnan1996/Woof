ac57cb7ce3489cd0a2860a61258f8f2aa55fe67a
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 18:36:13 +0530

    Woof

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+   Woof(10)
+
+def Woof(num):
+   for i in range(num):
+      print('Woof')
+
+if __name__ == "__main__":
+	main()
